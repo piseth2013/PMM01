29439ee13045c398e90f6e3711f2965f242935bb
seng piseth <128473317+piseth2013@users.noreply.github.com>
2025-06-20 13:09:16 +0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: '.',
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  }
+});
